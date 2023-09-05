b18c9660b810bb895c90cbde2dbf31e431ca2b24
Andreas Sommer <sommer@belsignum.com>
2023-09-05 13:26:08 +0200

    [FEATURE] OAuth2 Access Token Handling

--- Classes/Service/PardotService.php
+++ Classes/Service/PardotService.php
@@ -9,59 +9,68 @@ namespace Belsignum\Pardot\Service;
 
 use CyberDuck\Pardot\PardotApi;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class PardotService
 {
 
-	/**
-	 * Extension key
-	 */
-	public const EXTKEY = 'pardot';
+    /**
+     * Extension key
+     */
+    public const EXTKEY = 'pardot';
 
-	/**
-	 * @var PardotApi
-	 */
-	public $pardot;
+    /**
+     * @var PardotApi
+     */
+    public PardotApi $pardot;
 
-	/**
-	 * Extension Settings
-	 *
-	 * @var array
-	 */
-	protected $settings;
+    /**
+     * Extension Settings
+     *
+     * @var array
+     */
+    protected array $settings;
 
-	/**
-	 * initialize API
-	 */
-	public function __construct()
-	{
-		$this->getExtConfSettings();
+    /**
+     * initialize API
+     */
+    public function __construct()
+    {
+        $this->getExtConfSettings();
 
-		$this->pardot = GeneralUtility::makeInstance(
-			PardotApi::class,
-			$this->settings['email'],
-			$this->settings['password'],
-			$this->settings['user_key']
-		);
-	}
+        $this->pardot = GeneralUtility::makeInstance(
+            PardotApi::class,
+            $this->settings['clientId'],
+            $this->settings['clientSecret'],
+            $this->settings['redirectUri'],
+            $this->settings['businessUnitId'],
+            $this->settings['accessTokenStorage']
+        );
+    }
 
-	/**
-	 * get extension configuration settings
-	 * @return void
-	 */
-	public function getExtConfSettings()
-	{
-		$this->settings = GeneralUtility::makeInstance(ExtensionConfiguration::class)
-			->get('pardot');
-	}
+    /**
+     * get extension configuration settings
+     * @return void
+     */
+    public function getExtConfSettings()
+    {
+        $this->settings = GeneralUtility::makeInstance(ExtensionConfiguration::class)
+            ->get(self::EXTKEY);
 
-	/**
-	 * get the visitor ID set by cookie
-	 * @return string
-	 */
-	public function getVisitorId()
-	{
-		return $_COOKIE['visitor_id' . $this->settings['account']];
-	}
+        $this->settings['accessTokenStorage'] = Environment::getConfigPath() . $this->settings['accessTokenStorage'];
+    }
+
+    /**
+     * get the visitor ID set by cookie
+     * @return string
+     */
+    public function getVisitorId()
+    {
+        if ($this->settings['staticVisitorId'])
+        {
+            return $this->settings['staticVisitorId'];
+        }
+        return $_COOKIE['visitor_id' . $this->settings['account']];
+    }
 }
